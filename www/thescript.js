c9e12ab0081c0642d259e513ce69175e35a3df60
Kevin Lin <thekevinlin@utexas.edu>
2015-06-16 22:22:55 -0500

    screentest.html: Kill the drawer. Kill commented out HTMl5 canvas code. Adjust screen metric dump.

--- www/thescript.js
+++ www/thescript.js
@@ -37,28 +37,19 @@ function drawEverything() {
 	// layout for screen size < 800px or whatever arbitrary amount. Draw desktop otherwise.
 	stage.removeAllChildren();
 
-	drawIcons();
+	drawTestIcons();
 	infoDump();
-	drawDrawer();
+	drawDrawerIcon();
 
 	stage.update();
 }
 
-function drawIcons() {
-	var shield = new createjs.Text("\uE32A", "48px Material Icons", "black");
-	shield.x = 350;
-	shield.y = 100;
-	stage.addChild(shield);
-	// context.font = "48px Material Icons";
-	// context.fillText("\uE32A", 350, 100);
+function drawTestIcons() {
 
 	var card = new createjs.Shape();
 	card.graphics.beginFill('white').drawRoundRect(445, 100, 70, 98, 5);
 	card.shadow = new createjs.Shadow("black", 0, 1, 2);
 	stage.addChild(card);
-	// context.fillStyle = "white";
-	// context.rect(470, 100, 100, 100);
-	// context.fill();
 
 	var C = new createjs.Text("\u2663", "48px Roboto Condensed", "black");
 	C.x = 460;
@@ -67,28 +58,22 @@ function drawIcons() {
 	seven.x = 470;
 	seven.y = 100;
 	stage.addChild(C, seven);
-	// context.font = "48px Roboto Condensed";
-	// context.fillStyle = "black";
-	// context.fillText("7", 470, 100);
-	// context.fillText("\u2663", 460, 148);
 
 	var HA = new createjs.Text("A" + "\u2665", "48px Roboto Condensed", "red");
 	HA.x = 540;
 	HA.y = 100;
 	stage.addChild(HA);
-	// context.fillStyle = "red";
-	// context.fillText("A" + "\u2665", 540, 100);
 }
 
 function infoDump() {
 	var color = "black";
-	var font = "48px Roboto Condensed";
+	var font = "24px Roboto Condensed";
 
-	var dw = new createjs.Text("Device width: " + window.innerWidth, font, color);
+	var dw = new createjs.Text("Viewport width: " + window.innerWidth, font, color);
 	dw.x = 400;
 	dw.y = 350;
 
-	var dh = new createjs.Text("Device height: " + window.innerHeight, font, color);
+	var dh = new createjs.Text("Viewport height: " + window.innerHeight, font, color);
 	dh.x = 400;
 	dh.y = 400;
 
@@ -113,40 +98,15 @@ function infoDump() {
 	ash.y = 650;
 
 	stage.addChild(dw, dh, prtext, sw, sh, asw, ash);
-
-	// context.fillText("Device width: " + window.innerWidth, 400, 400);
-	// context.fillText("Device height: " + window.innerHeight, 400, 450)
-	// context.fillText("Pixel ratio: " + pr, 400, 500);
 }
 
-function drawDrawer() {
-	//Todo: DO THE THING
-	var drawerWidth = table.width * .15;
-	if (drawerWidth >= 200) {
-		var drawer = new createjs.Shape();
-		drawer.graphics.beginFill("white").drawRect(0,0,drawerWidth, table.height);
-		drawer.shadow = new createjs.Shadow("black", -10, 0, 80);
-		stage.addChild(drawer);
-
-		// context.rect(0,0,drawerWidth, table.height);
-		// context.fillStyle="white";
-		// context.shadowColor = "black";
-		// context.shadowBlur = 80;
-		// context.shadowOffsetX = -10;
-		// context.fill();
-	} else {
-		var drawerIcon = new createjs.Text("\uE88E", (48*pr) + "px Material Icons", "#F1F1F1");
-		drawerIcon.x = drawerIcon.y = 10*pr;
-		stage.addChild(drawerIcon);
-
-		// context.fillStyle="#F1F1F1";
-		// context.font = (48 * pr) + "px Material Icons";
-		// context.fillText("\uE88E", 10*pr, 58*pr);
-	}
+function drawDrawerIcon() {
+	var drawerIcon = new createjs.Text("\uE88E", (48*pr) + "px Material Icons", "#F1F1F1");
+	drawerIcon.x = drawerIcon.y = 10;
+	stage.addChild(drawerIcon);
 }
 
 window.addEventListener('resize', function() {
-
 	sizeCanvas();
 	drawEverything();
 });
