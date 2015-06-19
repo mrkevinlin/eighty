63049a3b03782c09271ba1d41d29d5291a13be82
Chris Roberts <randomnameftw@gmail.com>
2015-06-18 22:34:43 -0500

    add a page to test channel creation and message sending

--- appengine/main.py
+++ appengine/main.py
@@ -1,7 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from google.appengine.api.channel import channel
+import uuid
 
 app = Flask(__name__, static_url_path='')
 app.config['DEBUG'] = False
+
+my_uuid = unicode(uuid.uuid4())
+token = None
 
 
 @app.route('/')
@@ -19,3 +24,16 @@ def page_not_found(e):
 @app.errorhandler(403)
 def page_forbidden(e):
     return app.send_static_file('forbidden.html')
+
+@app.route('/channel')
+def channel_test():
+    global token
+    token = channel.create_channel(my_uuid)
+    return render_template('channeltest.html', token=token)
+
+@app.route('/taketurn')
+def sendmessage():
+    if token:
+        channel.send_message(token, 'Token is set to ' + token)
+    return 'channel communication!'
+
