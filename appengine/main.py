3d1046f23085596ac5ac72ff7af52a37eac6e67c
Chris Roberts <randomnameftw@gmail.com>
2015-06-20 02:03:35 -0500

    partially implement a simple simon says game with sessions and channels

--- appengine/main.py
+++ appengine/main.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, render_template
+from __future__ import print_function
+from flask import Flask, request, render_template, session, redirect, url_for
 from google.appengine.api.channel import channel
 import uuid
 
 app = Flask(__name__, static_url_path='')
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
+# DON'T COMMIT THIS
+app.secret_key = '\x97c\xf3\xcb0\x98WaGS\xcb\xbf\xaesX\x0e\x7f{\xeaK\xb6\xbaaU'
 
 my_uuid = unicode(uuid.uuid4())
 token = None
+ss_token = None
 
 
 @app.route('/')
@@ -25,15 +29,56 @@ def page_not_found(e):
 def page_forbidden(e):
     return app.send_static_file('forbidden.html')
 
-@app.route('/channel')
-def channel_test():
-    global token
-    token = channel.create_channel(my_uuid)
-    return render_template('channeltest.html', token=token)
+# simon says code
+ss_players = 0
+current_players = []
+LOGIN_FORM = '''
+            <form action="" method="post">
+                <p><input type=text name=username>
+                <p><input type=submit value=Login>
+            </form>
+            '''
 
-@app.route('/taketurn')
-def sendmessage():
-    if token:
-        channel.send_message(token, 'Token is set to ' + token)
-    return 'channel communication!'
+@app.route('/simonsays', methods=['GET','POST'])
+def simon_says():
+    global ss_players
+    if request.method == 'POST':
+        formname = request.form['username']
+        if formname in current_players:
+            # duplicate, REJECTED
+            return 'That username has already been taken<br/>' + LOGIN_FORM
+        else:
+            session['username'] = formname
+            current_players.append(formname)
+            ss_players += 1
+            return redirect(url_for('simon_says'))
+    if 'username' not in session:
+        # ask for signin
+        return LOGIN_FORM
+    else:
+        global ss_token
+        if not ss_token:
+            ss_token = start_simon_says()
+        return render_template('simonsays.html', players=ss_players,
+                token=ss_token, username=session['username'])
+
+def start_simon_says():
+    return channel.create_channel(my_uuid)
+
+@app.route('/simonsays/logoff')
+def ss_logoff():
+    global ss_players
+    name = session.pop('username', None)
+    if name:
+        ss_players -= 1
+    return redirect(url_for('simon_says'))
+
+@app.route('/simonsays/requestupdate')
+def ss_request_update():
+    update_requested = request.args['category']
+    if update_requested == 'players':
+        channel.send_message(ss_token, str(ss_players))
+    else:
+        abort(404)
+    return ('', 204)
 
