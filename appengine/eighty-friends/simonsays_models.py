22d3dade53a3701f4b8774a8a985edd624fab2ad
Chris Roberts <randomnameftw@gmail.com>
2015-06-27 04:53:01 -0500

    Remove unused functions from NDB models

--- appengine/eighty-friends/simonsays_models.py
+++ appengine/eighty-friends/simonsays_models.py
@@ -4,17 +4,9 @@ class Player(ndb.Model):
     name = ndb.StringProperty('name')
     channel_token = ndb.StringProperty('channel_token')
 
-    @classmethod
-    def get_player_names(cls):
-        return [x.name for x in cls.query()]
-
 class Game(ndb.Model):
     name = ndb.StringProperty('name')
     started = ndb.BooleanProperty('started', default=False)
     leader = ndb.StringProperty('leader')
     sequence_length = ndb.IntegerProperty('sequence_length', default=3)
     players = ndb.KeyProperty(kind=Player, repeated=True)
-
-    @classmethod
-    def get_default(cls):
-        return cls.query(cls.name == 'default')
